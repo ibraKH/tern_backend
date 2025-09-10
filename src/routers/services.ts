59e885b3074fa08f8ab24810e7f45a0735cc3210
Ibrahim Al-harthi <ibrakh80@gmail.com>
2025-09-10 16:14:52 +1000

    delete test

--- src/routers/services.ts
+++ src/routers/services.ts
@@ -1,10 +1,8 @@
 import express, { Request, Response } from 'express';
 import pool from '../database';
-import { log } from 'console';
 
 const router = express.Router();
 
-console.log('Services router loaded');
 
 
 export default router;
