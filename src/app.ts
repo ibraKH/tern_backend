eff7221846d0433ddc7fb57036b8db21147baf03
Ibrahim Al-harthi <ibrakh80@gmail.com>
2025-10-22 13:13:33 +1100

    remove console.log

--- src/app.ts
+++ src/app.ts
@@ -46,17 +46,12 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: (origin, callback) => {
-    console.log('CORS origin check:', origin);
-    console.log('Allowed origins:', allowedOrigins);
-    
+  origin: (origin, callback) => {    
     if (!origin) return callback(null, true);
     
     if (allowedOrigins.includes(origin)) {
-      console.log('Origin allowed:', origin);
       callback(null, true);
     } else {
-      console.log('Origin blocked:', origin);
       callback(new Error('Not allowed by CORS'));
     }
   },
